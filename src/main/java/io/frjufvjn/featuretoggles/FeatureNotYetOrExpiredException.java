ce653a8bc2078ef92aa3017ae911f6887c1defd1
jwpark7 <jwpark7@scglab.com>
2024-04-25 07:44:13 +0900

    chore: delete garbage

--- src/main/java/io/frjufvjn/featuretoggles/FeatureNotYetOrExpiredException.java
+++ src/main/java/io/frjufvjn/featuretoggles/FeatureNotYetOrExpiredException.java
@@ -2,8 +2,6 @@ package io.frjufvjn.featuretoggles;
 
 /**
  * 출시나 유지기간이 아닌 토글의 경우 예외
- *
- * @see <a href="https://scglab.atlassian.net/wiki/spaces/SCGLAB/pages/475332623/Feature+Toggles+Flag#%EA%B8%B0%EB%8A%A5">토글 기능 문서</a>
  */
 public class FeatureNotYetOrExpiredException extends RuntimeException {
     private static final long serialVersionUID = 5463461491849847646L;
