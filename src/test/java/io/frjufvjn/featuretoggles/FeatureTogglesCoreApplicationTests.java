95f85c3e522ce7ec621ea653c4a52885bace7c89
jwpark7 <jwpark7@scglab.com>
2024-04-24 21:39:44 +0900

    init

---
+++ src/test/java/io/frjufvjn/featuretoggles/FeatureTogglesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.frjufvjn.featuretoggles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureTogglesCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
