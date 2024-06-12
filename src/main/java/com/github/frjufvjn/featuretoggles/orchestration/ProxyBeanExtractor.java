93dde8573957d95c0c5a893a350476d3b11ad07d
jwpark7 <jwpark7@scglab.com>
2024-06-13 06:14:13 +0900

    build: 0.2.5-alpha

--- src/main/java/com/github/frjufvjn/featuretoggles/orchestration/ProxyBeanExtractor.java
+++ src/main/java/com/github/frjufvjn/featuretoggles/orchestration/ProxyBeanExtractor.java
@@ -1,10 +1,11 @@
 package com.github.frjufvjn.featuretoggles.orchestration;
 
 import com.github.frjufvjn.featuretoggles.FeatureOption;
-import org.springframework.aop.framework.AopProxyUtils;
+// import org.springframework.aop.framework.AopProxyUtils;
 
 public class ProxyBeanExtractor {
     public static FeatureOption getFeatureOptionIfProxyBean(Object proxy) {
-        return AopProxyUtils.ultimateTargetClass(proxy).getAnnotation(FeatureOption.class);
+        // return AopProxyUtils.ultimateTargetClass(proxy).getAnnotation(FeatureOption.class);
+        return null;
     }
 }
